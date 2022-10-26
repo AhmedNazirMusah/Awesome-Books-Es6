d19d79ecb7b30cd5252f4f4eb64d239629d2f130
ahmednazirmusah <ahmed999nazir@gmail.com>
2022-10-26 10:11:06 +0000

    date update

--- modules/date.js
+++ modules/date.js
@@ -2,7 +2,7 @@ import { DateTime } from '../node_modules/luxon/src/luxon.js';
 
 const setDate = () => {
   const datehandler = document.querySelector('.date');
-  datehandler.innerHTML += `<h2 class="dates">${DateTime.now().toISO()}<h2>`;
+  datehandler.innerHTML = `<h2 class="dates">${DateTime.now()}<h2>`;
 };
 
 export default setDate;
